0b1d59cc5c8dbdbf99034fd69f1f05df936cacaa
Maxime Cabanal <=>
2019-09-11 10:23:11 +0200

    Changing uniform sending to allow passing rvalue glm vectors

--- include/endora/endora_ecs_friendly.hpp
+++ include/endora/endora_ecs_friendly.hpp
@@ -64,7 +64,7 @@ static GLchar * filetobuf(const char *file) {
 /* ----------------------------- */
 
 // program part
-void check_shader_compile_status(shader_t shader, type_t type) {
+inline void check_shader_compile_status(shader_t shader, type_t type) {
     int compiled;
     glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled); endora_error("getting shader compile status");
     if (!compiled) {
@@ -78,7 +78,7 @@ void check_shader_compile_status(shader_t shader, type_t type) {
     }
 }
 
-shader_t create_shader(type_t type, char const * filename) {
+inline shader_t create_shader(type_t type, char const * filename) {
     shader_t shader = glCreateShader(type); endora_error("creating shader");
     GLchar * source = filetobuf(filename);
     glShaderSource(shader, 1, &source, nullptr); endora_error("specifying source");
@@ -89,7 +89,7 @@ shader_t create_shader(type_t type, char const * filename) {
     return shader;
 }
 
-void destroy_shader(shader_t shader) { glDeleteShader(shader); endora_error("destroying shader"); }
+inline void destroy_shader(shader_t shader) { glDeleteShader(shader); endora_error("destroying shader"); }
 
 template < typename ... Shaders >
 void attach_shaders(program_t program, shader_t shader, Shaders && ... shaders) {
@@ -111,9 +111,9 @@ void detach_shaders(program_t program, shader_t shader, Shaders && ... shaders) 
     }
 }
 
-void link_program(program_t program) { glLinkProgram(program); endora_error("linking program"); }
+inline void link_program(program_t program) { glLinkProgram(program); endora_error("linking program"); }
 
-void check_program_link_status(program_t program) {
+inline void check_program_link_status(program_t program) {
     int linked;
     glGetProgramiv(program, GL_LINK_STATUS, &linked); endora_error("getting program link status");
     if (!linked){
@@ -136,35 +136,36 @@ program_t create_program(Shaders && ... shaders) {
     link_program(program);
     check_program_link_status(program);
     detach_shaders(program, shaders...);
+    return program;
 }
 
-void destroy_program(program_t program) { glDeleteProgram(program); endora_error("destroying program"); }
+inline void destroy_program(program_t program) { glDeleteProgram(program); endora_error("destroying program"); }
 
-void use_program(program_t program) { glUseProgram(program); endora_error("using program"); }
+inline void use_program(program_t program) { glUseProgram(program); endora_error("using program"); }
 
 
 // vbo part
-buffer_t create_buffer() {
+inline buffer_t create_buffer() {
     buffer_t buffer;
     glGenBuffers(1, &buffer); endora_error("creating buffer");
     return buffer;
 }
 
-void destroy_buffer(buffer_t buffer) { glDeleteBuffers(1, &buffer); endora_error("destroying buffer"); }
+inline void destroy_buffer(buffer_t buffer) { glDeleteBuffers(1, &buffer); endora_error("destroying buffer"); }
 
-void bind_buffer(buffer_t buffer, type_t type) { glBindBuffer(type, buffer); endora_error("bind buffer"); }
+inline void bind_buffer(buffer_t buffer, type_t type) { glBindBuffer(type, buffer); endora_error("bind buffer"); }
 
 template < typename T >
 void set_buffer_data(type_t type, std::size_t size, T const * data, type_t usage) {
     glBufferData(type, size, data, usage); endora_error("Setting buffer data");
 }
 
-void format_buffer_index_data(GLuint index, int coordinate_size, type_t type, unsigned stride, void * pointer) {
+inline void format_buffer_index_data(GLuint index, int coordinate_size, type_t type, unsigned stride, void * pointer) {
     glEnableVertexAttribArray(index); endora_error("enable vertex attrib array");
     glVertexAttribPointer(index, coordinate_size, type, GL_FALSE, stride, pointer); endora_error("vertex attrib pointer format");
 }
 
-void allocate_buffer_storage(type_t type, std::size_t size, GLbitfield flags) {
+inline void allocate_buffer_storage(type_t type, std::size_t size, GLbitfield flags) {
     glBufferStorage(type, size, nullptr, flags); endora_error("allocating buffer storage");
 }
 
@@ -173,51 +174,51 @@ T * map_buffer_range(type_t type, GLintptr offset, std::size_t size, GLbitfield 
     return static_cast<T *>(glMapBufferRange(type, offset, size, access));
 }
 
-void unmap_buffer(type_t type) { glUnmapBuffer(type); endora_error("unmapping buffer"); }
+inline void unmap_buffer(type_t type) { glUnmapBuffer(type); endora_error("unmapping buffer"); }
 
 // vertex array
-vertex_array_t create_vertexarray() {
+inline vertex_array_t create_vertexarray() {
     vertex_array_t vertex_array;
     glGenVertexArrays(1, &vertex_array); endora_error("creating vertex array");
     return vertex_array;
 }
 
-void destroy_vertex_array(vertex_array_t vertex_array) { glDeleteVertexArrays(1, &vertex_array); endora_error("destroying vertex array"); }
+inline void destroy_vertex_array(vertex_array_t vertex_array) { glDeleteVertexArrays(1, &vertex_array); endora_error("destroying vertex array"); }
 
-void bind_vertex_array(vertex_array_t vertex_array) { glBindVertexArray(vertex_array); endora_error("binding vertex array"); }
+inline void bind_vertex_array(vertex_array_t vertex_array) { glBindVertexArray(vertex_array); endora_error("binding vertex array"); }
 
 // uniform
 
 /* This function does not affect current gl_program, so be sure to use the program before */
-uniform_t create_uniform(program_t program, char const * name) {
+inline uniform_t create_uniform(program_t program, char const * name) {
     uniform_t location = glGetUniformLocation(program, name); endora_error("getting uniform location: ", name);
     return location;
 }
 
 /* These functions do not affect current gl_program, so be sure to use the program before */
-void send_uniform(uniform_t uniform, int value) { glUniform1i(uniform, value); endora_error("sending uniform int value"); }
-void send_uniform(uniform_t uniform, float value) { glUniform1f(uniform, value); endora_error("sending uniform float value"); }
-void send_uniform(uniform_t uniform, glm::vec2 value) { glUniform2fv(uniform, 2, glm::value_ptr(value)); endora_error("sending uniform vec2 value"); }
-void send_uniform(uniform_t uniform, glm::vec3 value) { glUniform3fv(uniform, 3, glm::value_ptr(value)); endora_error("sending uniform vec3 value"); }
-void send_uniform(uniform_t uniform, glm::mat4 value) { glUniformMatrix4fv(uniform, 1, GL_FALSE, glm::value_ptr(value)); endora_error("sending uniform mat4 value"); }
+inline void send_uniform(uniform_t uniform, int value) { glUniform1i(uniform, value); endora_error("sending uniform int value"); }
+inline void send_uniform(uniform_t uniform, float value) { glUniform1f(uniform, value); endora_error("sending uniform float value"); }
+inline void send_uniform(uniform_t uniform, glm::vec2 value) { glUniform2f(uniform, value[0], value[1]); endora_error("sending uniform vec2 value"); }
+inline void send_uniform(uniform_t uniform, glm::vec3 value) { glUniform3f(uniform, value[0], value[1], value[2]); endora_error("sending uniform vec3 value"); }
+inline void send_uniform(uniform_t uniform, glm::mat4 value) { glUniformMatrix4fv(uniform, 1, GL_FALSE, glm::value_ptr(value)); endora_error("sending uniform mat4 value"); }
 
 // texture
-texture_t create_texture() {
+inline texture_t create_texture() {
     texture_t texture;
     glGenTextures(1, &texture); endora_error("creating texture");
     return texture;
 }
 
-void destroy_texture(texture_t texture) { glDeleteTextures(1, &texture); endora_error("destroying texture"); }
+inline void destroy_texture(texture_t texture) { glDeleteTextures(1, &texture); endora_error("destroying texture"); }
 
-void bind_texture(texture_t texture, type_t target) { glBindTexture(target, texture); endora_error("bind texture"); }
+inline void bind_texture(texture_t texture, type_t target) { glBindTexture(target, texture); endora_error("bind texture"); }
 
-void load_2D_surface(type_t target, unsigned width, unsigned height, void * pixels, type_t internal_format, type_t format, type_t type) {
+inline void load_2D_surface(type_t target, unsigned width, unsigned height, void * pixels, type_t internal_format, type_t format, type_t type) {
     glTexImage2D(target, 0, internal_format, width, height, 0, format, type, pixels);
     endora_error("Setting texture surface");
 }
 
-void bind_texture_slot(texture_t texture, type_t target, int slot) {
+inline void bind_texture_slot(texture_t texture, type_t target, int slot) {
     glActiveTexture(GL_TEXTURE0 + slot); endora_error("activating texture slot ", slot);
     bind_texture(texture, target);
 }
