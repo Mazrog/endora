4a9c515e8eba2b587ce12779038bc5c7fffc97f2
mazrog <maxime.cabanal@outlook.fr>
2019-09-14 22:47:33 +0200

    [Single-header] Adding framebuffer and uniform block support for single header impl

--- single-header/endora.hpp
+++ single-header/endora.hpp
@@ -8,8 +8,19 @@
 #include <cstdio>
 #include <iostream>
 #include <GL/glew.h>
-#include <glm/fwd.hpp>
+#include <array>
+
+#ifndef ENDORA_CUSTOM_TYPES
+#include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
+
+namespace {
+    using glm::vec2;
+    using glm::vec3;
+    using glm::mat4;
+}
+
+#endif // ENDORA_CUSTOM_TYPES
 
 #ifndef endora_error
 #define endora_error(message, ...) \
@@ -31,6 +42,53 @@ namespace {
 
 #endif
 
+/* Utilities */
+
+namespace endora::utils {
+
+template < typename Enum >
+constexpr inline auto enumvalue(Enum value) { return static_cast<std::underlying_type_t<Enum>>(value); }
+
+template < typename ... Types >
+struct types_wrapper {};
+
+namespace impl {
+    template < typename ... > struct packsize;
+
+    template < template < class ... > typename C, typename ... Types >
+    struct packsize<C<Types...>> { static constexpr std::size_t value = sizeof...(Types); };
+}
+
+template < typename T >
+constexpr std::size_t packsize_v = impl::packsize<T>::value;
+
+template < typename T >
+constexpr std::size_t packsize() { return packsize_v<T>; }
+
+template < typename T >
+constexpr std::size_t padding = 0;
+
+template <> constexpr std::size_t padding<float> = 4;
+template <> constexpr std::size_t padding<int> = 4;
+template <> constexpr std::size_t padding<::vec2> = 8;
+template <> constexpr std::size_t padding<::vec3> = 16;
+template <> constexpr std::size_t padding<::mat4> = 16;
+
+using Pair = std::pair<size_t, size_t>;
+
+template < typename T >
+constexpr Pair get_type_data() {
+    return Pair{sizeof(T), padding<T>};
+}
+
+template < typename ... > struct helper;
+template < typename ... Types > struct helper<types_wrapper<Types...>> {
+    static constexpr Pair value[sizeof...(Types)] { get_type_data<Types>() ... };
+};
+
+}
+
+// --------------------------------------
 // Implementation of basic opengl structs
 
 namespace endora::ecs {
@@ -42,6 +100,7 @@ using shader_t = GLuint;
 using uniform_t = GLint;
 using type_t = GLenum;
 using texture_t = GLuint;
+using framebuffer_t = GLuint;
 
 /* Utils */
 inline
@@ -250,6 +309,86 @@ inline void bind_texture_slot(texture_t texture, type_t target, int slot) {
     bind_texture(texture, target);
 }
 
+// fbo part
+inline framebuffer_t create_framebuffer() {
+    framebuffer_t framebuffer;
+    glGenFramebuffers(1, &framebuffer); endora_error("creating framebuffer");
+    return framebuffer;
 }
 
-#endif //FRESHWIND_ENDORA_HPP
+inline void destroy_framebuffer(framebuffer_t framebuffer) {
+    glDeleteFramebuffers(1, &framebuffer); endora_error("deleting framebuffer");
+}
+
+inline void bind_framebuffer(framebuffer_t framebuffer) {
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer); endora_error("binding framebuffer");
+}
+
+inline void set_framebuffer_2D_texture(texture_t texture, GLenum attachment) {
+    glFramebufferTexture2D(GL_FRAMEBUFFER, attachment, GL_TEXTURE_2D, texture, 0);
+    endora_error("setting 2D texture for framebuffer");
+}
+
+inline void set_draw_read_mode(GLenum draw_mode, GLenum read_mode) {
+    glDrawBuffer(draw_mode); endora_error("setting draw mode");
+    glReadBuffer(read_mode); endora_error("setting read mode");
+}
+
+// uniform block part
+
+template < typename T >
+struct UniformBlock {
+    unsigned id, binding;
+    T block;
+
+    UniformBlock(unsigned binding) : binding(binding) {
+        glGenBuffers(1, &id); endora_error("gen unif buffer");
+        glBindBuffer(GL_UNIFORM_BUFFER, id); endora_error("bind ubo");
+
+        constexpr std::size_t pack_size = utils::packsize_v<typename T::types>();
+
+        std::size_t offset = 0;
+
+        for (std::size_t i = 0; i < pack_size; ++i) {
+            auto [ size, alignment ] = utils::helper<typename T::types>::value[i]; // first -> sizeof / second -> alignment
+            const std::size_t ind = std::log2(alignment);
+
+            if (block.size & (alignment - 1)) {
+                offset = ((block.size >> ind) + 1) << ind;
+                block.fields_info[i] = { size, offset };
+                block.size = offset + size;
+            } else {
+                block.fields_info[i] = { size, block.size };
+                block.size += size;
+                offset = block.size;
+            }
+        }
+
+        glBufferData(GL_UNIFORM_BUFFER, block.size, nullptr, GL_STATIC_DRAW); endora_error("buffer data");
+        glBindBufferBase(GL_UNIFORM_BUFFER, binding, id);    endora_error("bind ubo binding point 0");
+    }
+
+    ~UniformBlock() { glDeleteBuffers(1, &id); endora_error("deleting buffer"); }
+
+    template < typename U >
+    void set(typename T::fields field, U && value) const {
+        utils::Pair const& pair = block.fields_info[utils::enumvalue(field)];
+        glBindBufferBase(GL_UNIFORM_BUFFER, binding, id); endora_error("bind ubo");
+        glBufferSubData(GL_UNIFORM_BUFFER, pair.second, pair.first, value);
+    }
+};
+
+#define UnifBlock(block_name, type_block, ...) \
+namespace ubo { \
+struct block_name { \
+    using types = type_block; \
+    enum class fields { __VA_ARGS__, field_count }; \
+    \
+    std::array<std::pair<size_t, size_t>, utils::enumvalue(fields::field_count)> fields_info; \
+    size_t size = 0; \
+}; \
+}
+
+}
+
+#endif //ENDORA_SINGLE_HEADER_HPP
